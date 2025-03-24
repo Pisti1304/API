c6a6175834b0d06c9f3d31c663448fd04c7585dd
Kerekes István <72573537332@szily.lan>
2025-03-24 09:16:36 +0100

    dbvezerlo elkezdve

---
+++ dbcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+class DBController
+{
+    private $csatl=null;
+
+    private $host ="localhost";
+
+    private $user = "root";
+
+    private $password = "";
+
+    private $database = "mobile";
+}
