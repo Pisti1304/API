a09025283fa8080c2f3c51f81102e376528f26f4
Kerekes István <72573537332@szily.lan>
2025-03-24 09:58:31 +0100

    dbcontroller elkeszult

--- dbcontroller.php
+++ dbcontroller.php
@@ -12,3 +12,39 @@ class DBController
 
     private $database = "mobile";
 }
+
+function __construct()
+{
+    $this->connectDB();
+}
+
+function connectDB(){
+    try
+    {
+        $this->conn = new PDO("mysql:host={$this->host}; dbname={$this->database} charset=utf8", $this->user; $this->password)
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $err)
+    {
+        die("Connect Failed".$err->GetMessage());
+    }
+
+function executeSelectQuery($query,$params=[])
+{
+    try
+    {
+        $stmt =$this->conn->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(PDOException $err)
+    {
+        die("Query Failed" . $err->GetMessage());
+    }
+}
+
+function closeDB()
+{
+    $this->conn=null;
+}
+}
---
+++ products.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Már 24. 09:26
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `mobile`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`) VALUES
+(1, 'Termmék1', 100.00, 'Ez egy autó polírozó'),
+(2, 'Termmék2', 110.00, 'Ez egy kárpittisztító'),
+(3, 'Termmék3', 199.00, 'Ez egy autó kulcstartó'),
+(4, 'Termmék4', 202.00, 'Ez egy autó indításgátló');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
