a4dd311f94be1662d9ec29ab560d75f7ad89738d
Kerekes István <72573537332@szily.lan>
2025-03-24 13:26:40 +0100

    kesz

---
+++ bestfilms/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @import url('css/styles.css');
+    </style>
+</head>
+<body>
+    <div class="header">
+        <img src="pics/OSCAR.jpg" alt="Oscar" class="oscar-image">
+        <div class="menu">
+            <a href="#films">Filmek</a>
+            <a href="#contact">Elérhetőségek</a>
+            <a href="#about">Rólunk</a>
+        </div>
+    </div>
+    <div id="films" class="container">
+        <h2>Filmek listája</h2>
+        <div class="films">
+            <div class="film">
+                <img src="pics/AmF.jpg" alt="Am.. Fic..">
+                <h3>American Fiction</h3>
+                <p>Nem mintha az American Ficiton története önmagában..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/ZuA.jpg" alt="Egy zuh....">
+                <h3>Ehy zuhanas anatómiája</h3>
+                <p>A sikeres német írónő francia férjével és látássér...</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Bar.jpg" alt="Barbie">
+                <h3>Barbie</h3>
+                <p>Van ám olyan világ,ahol minden csodálatos! mindig...</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Teszu.jpg" alt="Téli szü..">
+                <h3>Téli szünet</h3>
+                <p>Eljön a téliszünet, ám a jó nevű amerikai iskoláb..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Mae.jpg" alt="Mae..">
+                <h3>Maestro</h3>
+                <p>Egy film leonárd és Felicia bonyolult szerelméről,..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Oppe.jpg" alt="Oppe..">
+                <h3>Oppenheimer</h3>
+                <p>film J. Robert Oppenheimer fizikusról szól,aki..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Ele.jpg" alt="El.. el..">
+                <h3>Nora és Hae-sung gyerekkorukban a legjobb barátok</h3>
+                <p>Film  leírása  1</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Szegp.jpg" alt="Sze.. pár..">
+                <h3>Szegény párák</h3>
+                <p>Yorgos Lanthimos új filmje elképesztő történet Bel....</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Erdt.jpg" alt="Érd.. ter..">
+                <h3>Érdekvédelmi terület</h3>
+                <p>A Martin Amis angol író 2014-es regényét lazán ada..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/PePr.jpg" alt="PP">
+                <h3>Bobi Wine: The People's president</h3>
+                <p>A Martin Amis angol író 2014-es regényét lazán ada..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/EtM.jpg" alt="Et..  Me...">
+                <h3>The Eternal Memory</h3>
+                <p>Szomorú, de gyengéd  dokumentumfilm, üzenetében egy...</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/FD.jpg" alt="F D">
+                <h3>Four Daughters</h3>
+                <p>A fény és a sötétség között ott áll Olfa,  egytuné...</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/KaT.jpg" alt="K a tiger">
+                <h3>To kill a tiger</h3>
+                <p>Ranjit, egy indiai farmer vállalja élete harcát, a..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Dim.jpg" alt="20 Days">
+                <h3>20 Days in Mariupol</h3>
+                <p>Az Associated  Press(AP) ukrán újságíróinak csapat..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Ele.jpg" alt="Ele">
+                <h3>Elemi</h3>
+                <p>A Disney  és a Pixar új filmje, az ELEMI egy olyan..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Nim.jpg" alt="Nim">
+                <h3>Nimonia</h3>
+                <p>Egy középkori-futurisztikus királyság  polgárait az..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/PP.jpg" alt="Pókember..">
+                <h3>Pókember: A pókverzumon  át</h3>
+                <p>A Pókember - a Pókverzumon át remek  egyveleg:stil...</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/Robo.jpg" alt="Robo">
+                <h3>Roboálmok</h3>
+                <p>Miután a robot a munka ünnepén, a tengerparton tet..</p>
+                <a href="#">Részletek</a>
+            </div>
+            <div class="film">
+                <img src="pics/fszg.jpg" alt="A fiú ...">
+                <h3>A fiú és a szürke gém</h3>
+                <p>Egy mahito nevű fiatal fiú, aki az édesanyja után ..</p>
+                <a href="#">Részletek</a>
+            </div>
+        </div>
+    </div>
+    <div id="contact" class="container contact">
+        <h2>Elérhetőség</h2>
+        <p>Magasházy  épület...</p>
+    </div>
+    <div id="about" class="container about">
+        <h2>Oscarert mutatunk</h>
+        <p>Ezek vagyunk mi...</p>
+    </div>
+</body>
+</html>
