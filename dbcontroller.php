e551ec33dcfa262bb22e8d65228f4a305c244e98
Kerekes István <72573537332@szily.lan>
2025-03-24 10:32:35 +0100

    asd

--- dbcontroller.php
+++ dbcontroller.php
@@ -1,51 +1,50 @@
 <?php
-
 class DBController
 {
     private $conn=null;
+    private $host="localhost";
+    private $user="root";
 
-    private $host ="localhost";
-
-    private $user = "root";
-
-    private $password = "";
-
-    private $database = "mobile";
+    private $password="";
+    private $database ="mobile";
 
 
-function __construct()
-{
-    $this->connectDB();
-}
-
-function connectDB(){
-    try
+    function __construct()
     {
-        $this->conn = new PDO("mysql:host={$this->host}; dbname={$this->database} charset=utf8", $this->user, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $this->connectDB();
     }
-    catch(PDOException $err)
-    {
-        die("Connect Failed".$err->GetMessage());
-    }
-}
-function executeSelectQuery($query,$params=[])
-{
-    try
-    {
-        $stmt =$this->conn->prepare($query);
-        $stmt->execute($params);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch(PDOException $err)
-    {
-        die("Query Failed" . $err->GetMessage());
-    }
-}
 
-function closeDB()
-{
-    $this->conn=null;
-}
+    function connectDB()
+    {
+        try
+        {
+            $this->conn =new PDO("mysql:host={$this->host};
+            dbname={$this->database}; charset=utf8", $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $err)
+        {
+            die("Connection failed: " . $err->getMessage());
+        }
+    }
+
+    function executeSelectQuery($query, $params=[])
+    {
+        try
+        {
+            $stmt=$this->conn->prepare($query);
+            $stmt->execute($params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $err)
+        {
+            die("Query Failed" . $err->getMessage());
+        }
+    }
+
+    function closeDB()
+    {
+        $this->conn=null;
+    }
 }
 ?>
